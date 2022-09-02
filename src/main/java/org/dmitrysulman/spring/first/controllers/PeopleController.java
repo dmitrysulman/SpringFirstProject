a0ccb8552c1b76a2bc303c2fb03ebd1902b49944
Dmitry Sulman <dmitry.sulman@gmail.com>
2022-09-02 20:02:07 +0300

    Commit 1

---
+++ src/main/java/org/dmitrysulman/spring/first/controllers/PeopleController.java
@@ -0,0 +1,9 @@
+package org.dmitrysulman.spring.first.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/people")
+public class PeopleController {
+}
---
+++ src/main/java/org/dmitrysulman/spring/first/models/Person.java
@@ -0,0 +1,51 @@
+package org.dmitrysulman.spring.first.models;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+public class Person {
+    private int id;
+
+    @NotEmpty(message = "ФИО не должно быть пустым")
+    @Size(min = 3, max = 100, message = "Длина ФИО должна быть от 3 до 100")
+    private String fullName;
+
+    @NotEmpty(message = "Год рождения не должен быть пустым")
+    @Min(value = 1900, message = "Год рождение не должен быть меньше 1900")
+    private int yearOfBirth;
+
+    public Person(int id, String fullName, int yearOfBirth) {
+        this.id = id;
+        this.fullName = fullName;
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    public Person() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        this.yearOfBirth = yearOfBirth;
+    }
+}
